ae5a9623d488c9a11cb8c539a0fc954f8478867b
Miles <mcrawfor@D-69-91-162-181.dhcp4.washington.edu>
2009-04-14 15:41:44 -0700

    initial urlconf

---
+++ urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('uwregistry',
+    (r'^$', 'views.home'),
+)
--- views.py
+++ views.py
@@ -1,1 +1,4 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("First home")
