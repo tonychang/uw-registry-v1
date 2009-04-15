51a326fd28567e35e4e716ee0a67ce286368fc4a
Miles <mcrawfor@inkey.eplt.washington.edu>
2009-04-15 13:27:22 -0700

    make the nick validation refuse some reserved words

--- forms.py
+++ forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from uwregistry.models import Service
 
@@ -6,3 +7,9 @@ class ServiceForm(ModelForm):
     class Meta:
         model = Service
         exclude = ['owner', 'date_submitted', 'date_modified', 'status']
+
+    def clean_nickname(self):
+        nick = self.cleaned_data['nickname']
+        if nick == 'admin' or nick == 'service':
+            raise forms.ValidationError("The nickname '%s' is not usable." % nick)
+        return nick
