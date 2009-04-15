84c273d5a76a3a24b2e870447f077706d6432b60
Miles <mcrawfor@inkey.eplt.washington.edu>
2009-04-15 12:32:29 -0700

    lining up a submit view

--- urls.py
+++ urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('uwregistry',
     (r'^$', 'views.home'),
+    (r'^service/add/$', 'views.submit'),
+    (r'^(?P<nick>[-\w]+)/$', 'views.home'),
 )
--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
     return HttpResponse("First home")
+
+def submit(request):
+    return render_to_response("submit.html", {})
