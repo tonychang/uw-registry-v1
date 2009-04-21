7e0c39220c098df670eb99c7396a6a0bba87c185
Tony Chang <tony@mostlymodest.com>
2009-04-21 15:47:02 -0700

    Limit 20 services returned within the browse view

--- views.py
+++ views.py
@@ -25,7 +25,7 @@ def service(request, nick):
 
 
 def browse(request):
-    all_services = Service.objects.filter(status=Service.APPROVE_STAT)
+    all_services = Service.objects.filter(status=Service.APPROVE_STAT)[:20]
     return render_to_response("browse.html", {
         'all_services' : all_services,
         }, context_instance=RequestContext(request))
