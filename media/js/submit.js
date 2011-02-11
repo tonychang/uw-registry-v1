b626dbd49d5d1d53937acf36a0e8f284faff32df
Tony Chang <Tony@D-69-91-163-31.dhcp4.washington.edu>
2011-02-11 09:54:38 -0800

    Added footer and removed js code for submit page

--- media/js/submit.js
+++
@@ -1,6 +0,0 @@
-google.setOnLoadCallback(
-    function() {
-        $("input[name='in_development']").click(function() {
-		            $("input[name='doc_url']").toggle(this.checked);
-		  }
-    });
