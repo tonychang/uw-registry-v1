74399fb0a063ff80e1b8d9de95894dbc04a57a33
Chris Heiland <cheiland@u.washington.edu>
2011-02-14 11:13:04 -0800

    added login required back in

--- views.py
+++ views.py
@@ -140,7 +140,7 @@ def recent_activity(request):
         }, context_instance=RequestContext(request))
 
 
-#@login_required
+@login_required
 def mine(request):
     my_services = Service.objects.filter(owner=request.user)
     return render_to_response("mine.html", {
@@ -148,7 +148,7 @@ def mine(request):
         }, RequestContext(request))
  
 
-#@login_required
+@login_required
 def edit(request, nick):
     service = get_object_or_404(Service, nickname=nick, owner=request.user)
     if request.method == 'POST':
@@ -171,7 +171,7 @@ def edit(request, nick):
 
  
 
-#@login_required
+@login_required
 def submit(request):
 
     if request.method == 'POST':
